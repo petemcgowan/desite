91bbef8137fdde010c430d860a2e220640bf9b2d
Pete <peteburquette@gmail.com>
2020-11-06 03:46:58 +0000

    Missy Elliott remix

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
         djMixDownloads: false,
         techno: false,
         facts: [
+          {source:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/923486665&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true", name:"UltrafieldsMissy", height:  220},
           {source:"https://open.spotify.com/embed?uri=spotify:user:1163531592:playlist:3F06NSn0Zf6tIIRcraeKe9&amp;size=detail&amp;theme=light&amp;show-count=1", name:"name", height:  220},
           {source:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/668714891&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true", name:"name", height:  160},
           {source:"https://open.spotify.com/embed/track/1WXjNKjwp4iRKK0egpv7Rt", name:"name", height:  90},
