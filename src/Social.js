f807dadf9f5c8392debcd6ef67c3315df15013ab
Pete <peteburquette@gmail.com>
2021-03-09 16:28:00 +0000

    IG icon, etc

--- src/Social.js
+++ src/Social.js
@@ -8,7 +8,7 @@ class Social extends Component {
 
     return (
       <div className='Social' >
-        <SocialIcon className='Social' url="https://www.instagram.com/diplomaticenjoy" />
+        <SocialIcon className='Social' network="instagram" bgColor="#E1306C" url="https://www.instagram.com/diplomaticenjoy" />
         <SocialIcon className='Social' url="https://open.spotify.com/artist/0hFzAYqKLJFEBp5jd8RhmK" />
         <SocialIcon className='Social' url="https://www.soundcloud.com/diplomaticenjoy" />
         <SocialIcon className='Social' url="https://www.youtube.com/channel/UCcMegCxCOkDyzWcgtzYUSGg" />
