23d87346156028603564abeccf02be4845af7c99
Pete <peteburquette@gmail.com>
2021-04-06 18:07:46 +0100

    navbar changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = (props) => {
+  const pageLinks = props.pages.map((page) => (
+    <li className="nav-item" key={page.name}>
+      <NavLink exact to={`/pages/${page.name}`} className="nav-link">
+        {page.name}
+      </NavLink>
+    </li>
+  ));
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/pages">
+        Diplomatic Enjoy
+      </Link>
+
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">{pageLinks}</ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
