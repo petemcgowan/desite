e495479a2a6cb2a65aa9a117d1a9eef191b1c730
Pete <peteburquette@gmail.com>
2021-04-12 09:38:10 +0100

    changing gradient, json move

--- src/App.js
+++ src/App.js
@@ -1,114 +1,23 @@
-import React, { Component } from "react";
-import Routes from "./Routes";
-import Navbar from "./Navbar";
-import Social from "./Social";
+import React from "react";
+import Routes from "./components/Routes";
+import Navbar from "./components/Navbar";
+import Social from "./components/Social";
 
-// front page image imports
-import house from "./images/Housev4-(fish-eye,_noTitle500x500).jpg";
-import techno from "./images/ECPodCrowdControlv2500x500.jpg";
-import funk from "./images/funk400hi.jpg";
-import downloads from "./images/SushiDog400vhsfw.jpg";
+// note I could use state here, but it gets on the prop train, seems to be ok
+import { pages } from "./data/pages";
 
 import "./App.css";
 
-
-class App extends Component {
-
-  static defaultProps = {
-    downloadInfo: [ {
-      name: "House",
-    }],
-    pages: [
-      {
-        name: "House",
-        ageIsIrrelevant: 4,
-        src: house,
-        remixDownloads: false,
-        djMixDownloads: false,
-        techno: false,
-        facts: [
-          {source:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/991563850&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true", name:"CantDieSoul", height:  220},
-
-          {source:"https://open.spotify.com/embed?uri=spotify:user:1163531592:playlist:3F06NSn0Zf6tIIRcraeKe9&amp;size=detail&amp;theme=light&amp;show-count=1", name:"name", height:  220},
-
-          {source:"https://www.youtube.com/embed/yYHYUDogBJw",  name:"name", height:  220},
-
-          {source:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/923486665&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true", name:"UltrafieldsMissy", height:  220},
-
-          {source:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/668714891&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true", name:"name", height:  160},
-
-          {source:"https://open.spotify.com/embed/track/1WXjNKjwp4iRKK0egpv7Rt", name:"name", height:  80},
-
-//          {source:"https://open.spotify.com/embed/album/7zFl3kgDR4aQ8WDhvX8eWk", name:"name", height:  160},
-          {source:"https://open.spotify.com/embed/track/3rihedoymkkfQpa1J6vE0Q", name:"name", height:  80}
-        ]
-      },
-      {
-        name: "Techno",
-        ageIsIrrelevant: 3,
-        src: techno,
-        remixDownloads: false,
-        djMixDownloads: false,
-        techno: true,
-        facts: [
-          {source:"https://open.spotify.com/embed?uri=spotify:user:1163531592:playlist:6iqxF99IunM1sOPdqvQ3Q7",  name:"name", height:  220},
-
-          {source:"https://www.youtube.com/embed/lrjru5USh7Y", name:"name", height:  300},
-
-          {source:"https://www.youtube.com/embed/videoseries?list=PLMniNzct9KeZsJpQoF_pdXwB0NQsF94Sd",  name:"name", height:  220},
-
-          {source:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/697938109&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true", name:"name", height:  160}
-        ]
-      },
-      {
-        name: "DJ Downloads",
-        ageIsIrrelevant: 4,
-        src: downloads,
-        remixDownloads: true,
-        djMixDownloads: true,
-        techno: false,
-        facts: [
-        ]
-      },
-      {
-        name: "Funk",
-        ageIsIrrelevant: 4,
-        src: funk,
-        remixDownloads: false,
-        djMixDownloads: false,
-        techno: false,
-        facts: [
-          {source:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/720023266&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true", name:"name", height:  160},
-
-          {source:"https://open.spotify.com/embed/playlist/7CLPfDOCjgWcwo9g1tGPQo",  name:"name", height:  220},
-
-          {source:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/723474691&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true", name:"name", height:  160},
-
-          {source:"https://open.spotify.com/embed/track/0EmuEwPWgmhI5OXIQThH3t", name:"name", height:  90},
-
-          {source:"https://www.youtube.com/embed/videoseries?list=PLMniNzct9Kea6tS0LvYMAk_iw4IvrPH30",  name:"name", height:  160},
-
-          {source:"https://www.youtube.com/embed/stoRHlAhETs", name:"name", height:  160},
-
-          //          {source:"https://www.youtube.com/embed/DG5GnCwS_EA?rel=0", name:"name", height:  160},
-          {source:"https://www.mixcloud.com/widget/iframe/?hide_cover=1&feed=%2Fdiplomaticenjoy%2Funderground-funk-blackbyrds-singing%2F", name:"name", height:  120}
-        ]
-      }
-    ]
-  };
-
-
-  render() {
-    return (
-      <div>
-        <Navbar pages={this.props.pages} />
-        <div className='container'>
-          <Social />
-          <Routes pages={this.props.pages} />
-        </div>
+function App() {
+  return (
+    <div>
+      <Navbar pages={pages} />
+      <div className="container">
+        <Routes pages={pages} />
+        <Social />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
