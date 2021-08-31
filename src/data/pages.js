6f8c5cb458b0a7f0f90cdb079594d821ff44181f
Pete <peteburquette@gmail.com>
2021-08-31 16:53:54 +0100

    resuffling content, adding latest mix

--- src/data/pages.js
+++ src/data/pages.js
@@ -14,20 +14,7 @@ export const pages = [
     facts: [
       {
         source:
-          "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/991563850&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true",
-        name: "CantDieSoul",
-        height: 220,
-      },
-
-      {
-        source:
           "https://open.spotify.com/embed?uri=spotify:user:1163531592:playlist:3F06NSn0Zf6tIIRcraeKe9&amp;size=detail&amp;theme=light&amp;show-count=1",
-        name: "name",
-        height: 220,
-      },
-
-      {
-        source: "https://www.youtube.com/embed/yYHYUDogBJw",
         name: "name",
         height: 220,
       },
@@ -36,6 +23,12 @@ export const pages = [
         source:
           "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/923486665&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true",
         name: "UltrafieldsMissy",
+        height: 220,
+      },
+
+      {
+        source: "https://www.youtube.com/embed/yYHYUDogBJw",
+        name: "name",
         height: 220,
       },
 
@@ -52,7 +45,6 @@ export const pages = [
         height: 80,
       },
 
-      //          {source:"https://open.spotify.com/embed/album/7zFl3kgDR4aQ8WDhvX8eWk", name:"name", height:  160},
       {
         source: "https://open.spotify.com/embed/track/3rihedoymkkfQpa1J6vE0Q",
         name: "name",
@@ -68,16 +60,16 @@ export const pages = [
     techno: true,
     facts: [
       {
+        source: "https://www.youtube.com/embed/R7ezWNc6zaY",
+        name: "name",
+        height: 300,
+      },
+
+      {
         source:
           "https://open.spotify.com/embed?uri=spotify:user:1163531592:playlist:6iqxF99IunM1sOPdqvQ3Q7",
         name: "name",
         height: 220,
-      },
-
-      {
-        source: "https://www.youtube.com/embed/lrjru5USh7Y",
-        name: "name",
-        height: 300,
       },
 
       {
@@ -88,10 +80,9 @@ export const pages = [
       },
 
       {
-        source:
-          "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/697938109&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+        source: "https://www.youtube.com/embed/lrjru5USh7Y",
         name: "name",
-        height: 160,
+        height: 300,
       },
     ],
   },
@@ -112,13 +103,6 @@ export const pages = [
     facts: [
       {
         source:
-          "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/720023266&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
-        name: "name",
-        height: 160,
-      },
-
-      {
-        source:
           "https://open.spotify.com/embed/playlist/7CLPfDOCjgWcwo9g1tGPQo",
         name: "name",
         height: 220,
@@ -132,9 +116,9 @@ export const pages = [
       },
 
       {
-        source: "https://open.spotify.com/embed/track/0EmuEwPWgmhI5OXIQThH3t",
+        source: "https://www.youtube.com/embed/stoRHlAhETs",
         name: "name",
-        height: 90,
+        height: 160,
       },
 
       {
@@ -145,17 +129,9 @@ export const pages = [
       },
 
       {
-        source: "https://www.youtube.com/embed/stoRHlAhETs",
+        source: "https://open.spotify.com/embed/track/0EmuEwPWgmhI5OXIQThH3t",
         name: "name",
-        height: 160,
-      },
-
-      //          {source:"https://www.youtube.com/embed/DG5GnCwS_EA?rel=0", name:"name", height:  160},
-      {
-        source:
-          "https://www.mixcloud.com/widget/iframe/?hide_cover=1&feed=%2Fdiplomaticenjoy%2Funderground-funk-blackbyrds-singing%2F",
-        name: "name",
-        height: 120,
+        height: 90,
       },
     ],
   },
